6b3d25fa2698a961ed33b5031748b7664cb513cf
Je-chan <jkyyc3@gmail.com>
2022-11-19 22:58:09 +0900

    IMPLEMENT : Mark up file rendering

--- blog/pages/posts/[id].js
+++ blog/pages/posts/[id].js
@@ -1,32 +1,34 @@
-import Head from "next/head"
+import Head from "next/head";
 import Layout from "../../components/Layout";
-import { getAllPostIds, getPostData } from '../../lib/posts';
+import { getAllPostIds, getPostData } from "../../lib/posts";
 
 export async function getStaticPaths() {
-	const paths = getAllPostIds();
-	return {
-		paths,
-		fallback: false,
-	};
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
 }
-
 
 export async function getStaticProps({ params }) {
-	const postData = getPostData(params.id);
-	return {
-		props: {
-			postData,
-		},
-	};
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
 }
 
-export default  function Post() {
-	return (
-	<Layout>
-		<Head>
-			<h1>첫번째 글</h1>
-		</Head>
-		<h1>첫번째 글</h1>
-	</Layout>
-	)
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
 }
